034e2382ea682c130aad4a59d5d7fa83744d5d6e
zia <ziamalhi1234@gmail.com>
2024-09-11 04:44:27 +0500

    basics

--- Question Solving/Question.js
+++ Question Solving/Question.js
@@ -1,45 +1,54 @@
-// 1 Question:
+// --------------------------1 Question:-----------------------------------
 // Write a JavaScript function that demonstrates the use of both let and const variables.
 // 	Declare a variable using let, assign it a value, and then update the value.
-// 	Declare a variable using const, assign it a value, and try to update the value.
 // 	Use console.log to display the values before and after the updates.
 
-function age(){
-    var myAge = 20;
-    console.log(myAge)
-    var myAge = 24;
-    return myAge;
+function age() {
+  let myAge = 20;
+  console.log(myAge);
+  myAge += 2;
+  return myAge;
 }
 console.log(age());
 
+// 	Declare a variable using const, assign it a value, and try to update the value.
 
+function year() {
+  const year = 2020;
+  console.log();
+  //   year += 4;
+}
+console.log(year());
 
-// 2 Question:
+// --------------------------2 Question:-----------------------------------
 // Write a JavaScript function that takes an array as an argument. Inside the function:
 // 	console.log an element along with its index.
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+function numberArray() {
+  let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // 	Add a new value to the array and console.log
+  console.log(array);
+  array[10] = 22;
+  console.log(array.length);
+  return array;
+}
 
-console.log(array)
-// 	Add a new value to the array and console.log
-array[10] = 22;
 // 	console.log the new length of the array.
-console.log(array.length)
+console.log(numberArray());
 
-
-
-
-// 3 Question:	
+// --------------------------3 Question:-----------------------------------
 // Write a JavaScript program that creates an object. Inside the object:
 // 	Access and console.log each key and its value.
 let person = {
-    name: "John Doe",
-    age: 20,
-    city: "New York"
-}
-console.log(person);
+  name: "John Doe",
+  age: 20,
+  city: "New York",
+};
+console.log("name = ", person.name);
+console.log("age = ", person.age);
+console.log("city = ", person.city);
 
 // 	Add a new key-value pair to the object.
-person['role'] = "Programer";
+person["role"] = "Programer";
 
 // 	console.log the updated object.
-console.log(person);
+console.log("role = ", person.role);
